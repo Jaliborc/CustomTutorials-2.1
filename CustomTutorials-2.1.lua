190f1d21682b3ee8a843cd07d41a88c514985c5b
Joao Cardoso <jaliborc@arenajunkies.com>
2022-09-03 02:49:10 +0100

    New year

--- CustomTutorials-2.1.lua
+++ CustomTutorials-2.1.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2010-2021 João Cardoso
+Copyright 2010-2022 João Cardoso
 CustomTutorials is distributed under the terms of the GNU General Public License (or the Lesser GPL).
 This file is part of CustomTutorials.
 
