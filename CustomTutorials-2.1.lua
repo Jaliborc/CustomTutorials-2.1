9e535b05859c4cb3eee2e5681663155ac61d64b8
Joao Cardoso <442637+Jaliborc@users.noreply.github.com>
2023-02-07 12:32:57 +0100

    Year update

--- CustomTutorials-2.1.lua
+++ CustomTutorials-2.1.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2010-2022 João Cardoso
+Copyright 2010-2023 João Cardoso
 CustomTutorials is distributed under the terms of the GNU General Public License (or the Lesser GPL).
 This file is part of CustomTutorials.
 
